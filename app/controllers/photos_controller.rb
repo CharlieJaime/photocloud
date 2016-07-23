a451e79bec1bdfca25bddba4512eeec0ca7a88c8
Charlie Jaime <baroonie10@gmail.com>
2016-07-23 18:03:34 -0500

    new/create actions and views, _form partial

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,24 @@
 class PhotosController < ApplicationController
+  # before_action :find_photo
   def index
 
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+  end
+
+  private
+
+  def find_photo
+
+  end
+
+  def photo_params
+    params.require(:photo).permit(:title, :description)
   end
 end
