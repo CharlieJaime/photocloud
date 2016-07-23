2bf4cf65bf7c4a48cb87b70146ce462c4ce736cc
Charlie Jaime <baroonie10@gmail.com>
2016-07-23 18:17:50 -0500

    show index actions and views, before_action for find_photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,11 @@
 class PhotosController < ApplicationController
-  # before_action :find_photo
-  def index
+  before_action :find_photo, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @photos = Photo.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -10,12 +14,18 @@ class PhotosController < ApplicationController
 
   def create
     @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to @photo, notice: "Successfully uploaded"
+    else
+      render 'new'
+    end
   end
 
   private
 
   def find_photo
-
+    @photo = Photo.find(params[:id])
   end
 
   def photo_params
